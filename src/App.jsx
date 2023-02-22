b40359cf5fa26358416bf74e758b086e518ba082
SammCro <iso.oztrk@outlook.com>
2023-02-22 11:49:42 +0300

    added style.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import AddAnimal from "./AddAnimal";
 
+import './Styles/App.css'
+
 
 function randomAnimal () {
   const animals = ['bird','cat','cow','dog','gator','horse'];
@@ -20,9 +22,9 @@ function App() {
   })
 
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}> Click me! </button>
-      <h1>{renderedAnimal}</h1>
+      <h1 className="animal-list">{renderedAnimal}</h1>
     </div>
   );
 }
